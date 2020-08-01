f2ed70e311643a7500376d698384787da67a58b4
kenken <kenken@financie.jp>
2020-08-01 14:45:46 +0900

    next

--- src/main/kotlin/ClosedRange.kt
+++ src/main/kotlin/ClosedRange.kt
@@ -1,4 +1,4 @@
-class ClosedRange(val lowerEndPoint: Int, val upperEndPoint: Int) {
+data class ClosedRange(val lowerEndPoint: Int, val upperEndPoint: Int) {
     override fun toString(): String {
         return "[$lowerEndPoint,$upperEndPoint]"
     }
--- src/test/kotlin/ClosedRangeTest.kt
+++ src/test/kotlin/ClosedRangeTest.kt
@@ -1,37 +1,36 @@
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class ClosedRangeTest {
     @Test
     fun `整数閉区間オブジェクトは、下端点を持っていること`() {
-        val actual = ClosedRange(1, 2)
-
-        assertEquals(1, actual.lowerEndPoint)
+        val sut = ClosedRange(1, 2)
+        assertEquals(1, sut.lowerEndPoint)
     }
 
     @Test
     fun `整数閉区間オブジェクトは、上端点を持っていること`() {
-        val actual = ClosedRange(1, 2)
-
-        assertEquals(2, actual.upperEndPoint)
+        val sut = ClosedRange(1, 2)
+        assertEquals(2, sut.upperEndPoint)
     }
 
     @Test
     fun `下端点が1、上端点が2の整数閉区間オブジェクトは、［1,2］を返す`() {
         val sut = ClosedRange(1, 2)
-
-        val actual = sut.toString()
-
-        assertEquals("[1,2]", actual)
+        assertEquals("[1,2]", sut.toString())
     }
 
     @Test
     fun `下端点が2、上端点が3の整数閉区間オブジェクトは、［2,3］を返す`() {
         val sut = ClosedRange(2, 3)
+        assertEquals("[2,3]", sut.toString())
+    }
 
-        val actual = sut.toString()
-
-        assertEquals("[2,3]", actual)
+    @Test
+    fun `下端点が2、上端点が1の整数閉区間オブジェクトは生成できないこと`() {
+        assertThrows<IllegalArgumentException> {
+            ClosedRange(2, 1)
+        }
     }
 }
