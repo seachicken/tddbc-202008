82d734078161a39e017400e34ca46257335469ae
s-tanaka <seito.tanaka.dev@gmail.com>
2020-08-01 16:25:13 +0900

    finish

--- src/test/kotlin/ClosedRangeTest.kt
+++ src/test/kotlin/ClosedRangeTest.kt
@@ -1,31 +1,16 @@
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
-import org.junit.jupiter.api.assertThrows
 
 class ClosedRangeTest {
-    @Test
-    fun `整数閉区間オブジェクトは、下端点を持っていること`() {
-        val sut = ClosedRange(1, 2)
-        assertEquals(1, sut.lowerEndPoint)
-    }
-
-    @Test
-    fun `整数閉区間オブジェクトは、上端点を持っていること`() {
-        val sut = ClosedRange(1, 2)
-        assertEquals(2, sut.upperEndPoint)
-    }
-
-    @Test
-    fun `下端点が1、上端点が2の整数閉区間オブジェクトは、［1,2］を返す`() {
-        val sut = ClosedRange(1, 2)
-        assertEquals("[1,2]", sut.toString())
-    }
-
-    @Test
-    fun `下端点が2、上端点が3の整数閉区間オブジェクトは、［2,3］を返す`() {
-        val sut = ClosedRange(2, 3)
-        assertEquals("[2,3]", sut.toString())
+    @Nested
+    @DisplayName("整数閉区間オブジェクトは、文字列表現を返すこと")
+    inner class WhenToString {
+        @Test
+        fun `下端点が1、上端点が2の整数閉区間オブジェクトは、［1,2］を返す`() {
+            val sut = ClosedRange(1, 2)
+            assertEquals("[1,2]", sut.toString())
+        }
     }
 
     @Test
